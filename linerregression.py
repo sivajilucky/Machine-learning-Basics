5c29ea055df94085e3e2afdae05691fe81d4931e
sivaji <you@example.com>
2021-09-16 20:23:27 +0530

    added new linearregression code

---
+++ linerregression.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import linear_model
+import csv
+
+df=pd.read_csv("E:\homeprices.csv.xlsx",encoding='ISO-8859-1')
+print(df)
